41b463777db83629d4fe6168fc89dc1e1582c640
jmalabs <johnrichard.malabanan.0916@gmail.com>
2019-05-22 00:32:22 +0800

    project setup

--- App.Manager/App.Manager.WebUi/Controllers/ServersController.cs
+++ App.Manager/App.Manager.WebUi/Controllers/ServersController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.DirectoryServices;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -11,5 +13,33 @@ namespace App.Manager.WebUi.Controllers
     [ApiController]
     public class ServersController : ControllerBase
     {
+        private DirectoryEntry websiteEntry = null;
+        internal const string IIsWebServer = "IIsWebServer";
+
+        [HttpGet]
+        public List<string> GetServers()
+        {
+
+            DirectoryEntry Services = new DirectoryEntry("IIS://localhost/W3SVC");
+            IEnumerator ie = Services.Children.GetEnumerator();
+            DirectoryEntry Server = null;
+            List<string> sites = new List<string>();
+
+            // find iis website
+            while (ie.MoveNext())
+            {
+                Server = (DirectoryEntry)ie.Current;
+                if (Server.SchemaClassName == IIsWebServer)
+                {
+                    //// "ServerComment" means name
+                    //if (Server.Properties["ServerComment"][0].ToString() == name)
+                    //{
+                    sites.Add(Server.Properties["ServerComment"][0].ToString());
+                    //}
+                }
+            }
+
+            return sites;
+        }
     }
 }
